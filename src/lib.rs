98fdf7a59c84a57f55c6ab3faa32347293299c67
kino-ma <kino.ma.ms@gmail.com>
2020-09-03 23:08:08 +0900

    [update] parse functions and their test

--- src/lib.rs
+++ src/lib.rs
@@ -29,13 +29,17 @@ pub fn parse_fbs(data: &str) -> serde_json::Result<BlindSignature> {
 }
 
 pub fn parse_pubkey(data: &str) -> Result<RSAPublicKey, rsa::errors::Error> {
-    RSAPublicKey::from_pkcs8(data.as_bytes())
+    let judge_pubkey = pem::parse(data).expect("failed to parse judge public keypem");
+    RSAPublicKey::from_pkcs8(&judge_pubkey.contents)
 }
 
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    static PUBLIC_KEY: &str = "test_data/public_key.pem";
+    static SIGNATURE: &str = "test_data/signature.txt";
 
     #[test]
     fn should_parse_json() {
@@ -52,9 +56,29 @@ mod tests {
         let data = serde_json::to_string(&json_data)
             .expect("failed to convert to string");
 
-        let parsed = parse_json(&data)
+        let parsed = JsonData::from_str(&data)
             .expect("failed to parse");
         
         assert_eq!(json_data, parsed);
+    }
+
+    #[test]
+    fn should_parse_pubkey() {
+        let public_key = read_or_panic(PUBLIC_KEY);
+        parse_pubkey(&public_key)
+            .expect("failed to parse pubkey pkcs8");
+    }
+
+    #[test]
+    fn should_parse_fbs() {
+        let signature = read_or_panic(&SIGNATURE);
+        parse_fbs(&signature)
+            .expect("failed to parse fair-blind-signature");
+    }
+
+    fn read_or_panic(file: &str) -> String {
+        std::fs::read_to_string(file)
+            .map_err(|e| format!("failed to read file {}: {}", file, e))
+            .unwrap()
     }
 }
