4df445b392b0af751c17be744aed90836cb63f85
kino-ma <kino.ma.ms@gmail.com>
2020-09-03 22:11:29 +0900

    [add] JSON parsing function

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,24 @@
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct JsonData {
+    pub fair_blind_signature: String,
+    pub pubkey: String,
+    pub signature: String,
+    pub signed: SignedData,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct SignedData {
+    pub data: String,
+    pub random: u32,
+}
+
+pub fn parse_json(data: &str) -> serde_json::Result<JsonData> {
+    serde_json::from_str(data)
+}
+
+
 #[cfg(test)]
 mod tests {
     #[test]
